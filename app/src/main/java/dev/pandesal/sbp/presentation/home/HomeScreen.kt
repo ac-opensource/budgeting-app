a2771384061a7c875f41c347e5fccbf57e5f9ca5
andrew <aarconcepcion@gmail.com>
2025-05-18 10:43:48 +0800

    fix: missing import for wrapContentSize

--- app/src/main/java/dev/pandesal/sbp/presentation/home/HomeScreen.kt
+++ app/src/main/java/dev/pandesal/sbp/presentation/home/HomeScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CloseFullscreen
