15d4e0e4619009dbbd323dbf9f51c9811fbb536a
andrew <aarconcepcion@gmail.com>
2025-05-18 23:40:10 +0800

    fix: showTemplatePrompt condition change, fix wrong import in SquigglyDivider.kt

--- app/src/main/java/dev/pandesal/sbp/presentation/components/SquigglyDivider.kt
+++ app/src/main/java/dev/pandesal/sbp/presentation/components/SquigglyDivider.kt
@@ -5,8 +5,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.StrokeCap
 import androidx.compose.ui.unit.dp
 
 @Composable
