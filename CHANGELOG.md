7c2a370209c68437649395947af2605377e6f9c4
Andrew <a-ar@outlook.com>
2025-05-19 16:42:15 +0800

    feat(recurring): persist transactions using Room

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.25.0] - 2025-05-19
+### Added
+- Recurring transactions are now persisted using Room.
+
 
 ## [0.24.0] - 2025-05-19
 ### Added
---
+++ app/src/main/java/dev/pandesal/sbp/data/dao/RecurringTransactionDao.kt
@@ -0,0 +1,20 @@
+package dev.pandesal.sbp.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import dev.pandesal.sbp.data.local.RecurringTransactionEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecurringTransactionDao {
+    @Query("SELECT * FROM recurring_transactions")
+    fun getRecurringTransactions(): Flow<List<RecurringTransactionEntity>>
+
+    @Upsert
+    suspend fun insert(value: RecurringTransactionEntity)
+
+    @Delete
+    suspend fun delete(value: RecurringTransactionEntity)
+}
